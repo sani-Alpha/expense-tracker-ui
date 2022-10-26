4e6e58c6b6c2e5d6d7cb4701cdd71e85fa052371
Sanidhya Rai <sanidhya.rai@wolkus.com>
2022-10-26 22:49:53 +0530

    adding custom hook for https requests

--- src/client/hooks/use-http.js
+++ src/client/hooks/use-http.js
@@ -1,34 +1,37 @@
-import {useState} from 'react';
+import {useState, useCallback} from 'react';
 
 /**
  * @option {Object} - contains url, headers, method and body
  * @callback {method} - to send data back to parent invocation
  */
-const useHTTP = (options, callback) => {
+const useHTTP = callback => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const makeRequest = async () => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(options.url, {
-        method: options?.method ? options.method : 'GET',
-        body: options?.body ? JSON.stringify(options.body) : null,
-        headers: options?.headers ? options.headers : {}
-      });
+  const makeRequest = useCallback(
+    async options => {
+      setIsLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(options.url, {
+          method: options?.method ? options.method : 'GET',
+          body: options?.body ? JSON.stringify(options.body) : null,
+          headers: options?.headers ? options.headers : {}
+        });
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
+        if (!response.ok) {
+          throw new Error('Request failed!');
+        }
+
+        const data = await response.json();
+        callback(data);
+      } catch (err) {
+        setError(err.message || 'Something went wrong!');
       }
-
-      const data = await response.json();
-      callback(data);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
+      setIsLoading(false);
+    },
+    [callback]
+  );
 
   return {isLoading, error, makeRequest};
 };
